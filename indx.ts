e0c2d29fef4f636563cb35d0cfa6615bc147a3ac
One-andOnly <162917702+One-andOnly@users.noreply.github.com>
2024-03-24 01:47:50 +0500

    Number Guessing Game

--- indx.ts
+++ indx.ts
@@ -1,3 +1,23 @@
-console.log("test  repo");
-console.log("hello");
+#! /sr/bin/env node
+
+import inquirer from "inquirer";
+
+// first to generate a random number from computer
+
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+
+// Take a input from a user
+
+const userAns = await inquirer.prompt([
+  { message: "Please!  Gave a number between 1 to 10 :", name: "userNumber" ,type:"number" }
+]);
+
+// check whetheer a number is same or not 
+if (userAns.userNumber ==  randomNumber ){ 
+    console.log("Wow! you guess a right number ");
+    
+}
+else { console.log("you guess wrong the right number is  "  , randomNumber);
+}
+
 
